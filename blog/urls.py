224d9e70d76c994bb290615368f890e67968336c
kwj2020-hub <kuj4455@gmail.com>
2022-02-09 18:08:08 +0900

    CBV로 블로그 포스트 상세 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),   # URL 끝이 /blog/일 때는 PostList 클래스로 처리하도록 수정
-    path('<int:pk>/', views.single_post_page), # blog/views.py의 single_post_page() 함수에 정의된 대로 처리
+    # path('<int:pk>/', views.single_post_page), # blog/views.py의 single_post_page() 함수에 정의된 대로 처리
     # path('', views.index),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
-from django.views.generic import ListView   # ListView 클래스를 임포트하여 CBV 사용 준비 완료!
+# from django.shortcuts import render
+from django.views.generic import ListView, DetailView   # ListView와 DetailView 클래스를 임포트하여 CBV 사용 준비 완료!
 from .models import Post
 
 class PostList(ListView):   # index() 함수를 대체
     model = Post
     ordering = '-pk'
     # template_name = 'blog/post_list.html'   # template_name의 URL을 수동으로 지정
+
+class PostDetail(DetailView):
+    model = Post
 
 # 기존 FBV 스타일의 함수는 주석 처리
 # def index(request):
@@ -20,14 +23,14 @@ class PostList(ListView):   # index() 함수를 대체
 #         }
 #     )
 
-def single_post_page(request, pk):  # blog/urls.py에서 불러올 함수
-    post = Post.objects.get(pk=pk)  # pk는 primary key의 약자로
-    # 해당 함수의 매개변수로 지정하여 Post 모델의 pk 필드 값이 매개변수로 받은 pk와 같은 레코드를 가져오라는 의미
-
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post' : post,
-        }
-    )
+# def single_post_page(request, pk):  # blog/urls.py에서 불러올 함수
+#     post = Post.objects.get(pk=pk)  # pk는 primary key의 약자로
+#     # 해당 함수의 매개변수로 지정하여 Post 모델의 pk 필드 값이 매개변수로 받은 pk와 같은 레코드를 가져오라는 의미
+#
+#     return render(
+#         request,
+#         'blog/post_detail.html',
+#         {
+#             'post' : post,
+#         }
+#     )
